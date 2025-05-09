8727d9b76dfac243405d96bc34f8305e63785760
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2025-05-09 16:33:08 +0500

    Previous games work

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -9,33 +9,71 @@ router.get("/player", authenticate, async (req, res) => {
 
     const user = req.user;
 
+    interface Player {
+        id: number;
+        username: string;
+        score: number;
+    }
+
+    interface Game {
+        gameID: number;
+        players: Player[];
+        winner: string;
+        startedAt: Date;
+    }
+
     const games = await pool.request()
         .input("playerID", user.id)
-        .query("SELECT G.gameID, P.playerID, G.winner, P.username, GP.score, G.startedAt FROM GamePlayers GP\
+        .query<
+            {
+                gameID: number,
+                playerID: number,
+                username: string,
+                score: number,
+                winner: string,
+                startedAt: Date,
+            }
+        >("\
+            SELECT G.gameID, P.playerID, P.username, G.startedAt, GP.score, (SELECT TOP 1 username FROM Players WHERE playerID = G.winner) as winner\
+            FROM Players P\
+            JOIN GamePlayers GP ON P.playerID = GP.PlayerID\
             JOIN Games G ON GP.GameID = G.GameID\
-            JOIN Players P ON GP.PlayerID = P.playerID\
-            WHERE GP.PlayerID = @playerID")
-
-    // console.log(games.recordset);
+            WHERE GP.GameID IN (\
+                SELECT GameID FROM GamePlayers\
+                WHERE PlayerID = @playerID\
+            )\
+            "
+        );
 
     if (games.recordset.length === 0) {
         return res.status(404).json({ message: "No games found for this player." });
     }
 
-    const gameData = games.recordset.map((game) => {
-        return {
-            gameID: game.gameID,
-            playerID: game.playerID,
+    const gameMap = new Map<number, Game>();
+    
+    games.recordset.forEach((game) => {
+        if (!gameMap.has(Number(game.gameID))) {
+            gameMap.set(Number(game.gameID), {
+                gameID: game.gameID,
+                players: [],
+                winner: game.winner,
+                startedAt: game.startedAt
+            });
+        }
+
+        gameMap.get(Number(game.gameID))?.players.push({
+            id: game.playerID,
             username: game.username,
-            score: game.score,
-            winner: game.winner,
-            startedAt: game.startedAt,
-        };
+            score: game.score
+        });
     });
-
+    
+    const gameData = Array.from(gameMap.values());
+    
     console.log(gameData);
-
-    return res.status(200).json(gameData);
+    return res.status(200).json({
+        gameData: gameData,
+    });
 });
 
 export default router;
